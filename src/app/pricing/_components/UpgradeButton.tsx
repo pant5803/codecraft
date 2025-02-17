ef7a3d248d1841eca9288b1663d544deb734e64b
pant5803 <121814461+pant5803@users.noreply.github.com>
2025-02-17 19:12:28 +0530

    done

---
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -0,0 +1,19 @@
+import { Zap } from "lucide-react";
+import Link from "next/link";
+
+export default function UpgradeButton() {
+  const CHEKOUT_URL =
+    "https://codehazy07.lemonsqueezy.com/buy/f1620004-fbb3-44bd-a7b2-f85738888034";
+
+  return (
+    <Link
+      href={CHEKOUT_URL}
+      className="inline-flex items-center justify-center gap-2 px-8 py-4 text-white 
+        bg-gradient-to-r from-blue-500 to-blue-600 rounded-lg 
+        hover:from-blue-600 hover:to-blue-700 transition-all"
+    >
+      <Zap className="w-5 h-5" />
+      Upgrade to Pro
+    </Link>
+  );
+}
